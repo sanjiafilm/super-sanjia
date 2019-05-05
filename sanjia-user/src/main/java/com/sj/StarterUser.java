26dc1291b6fca434561b6d9c5607a774f6543e04
tarena <tarena@tarena-PC>
2019-05-05 13:38:34 +0800

    13:38

--- sanjia-user/src/main/java/com/sj/StarterUser.java
+++ sanjia-user/src/main/java/com/sj/StarterUser.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterUser {
 	public static void main(String[] args) {
 		//tesssst111111111122
+		//333
 		SpringApplication.run(StarterUser.class, args);
 	}
 }
