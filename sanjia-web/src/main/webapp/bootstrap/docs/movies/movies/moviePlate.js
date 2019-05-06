aa6b8e9d860d826d5dac25baffce6af133f5277f
park <1111@aa.cp>
2019-05-06 20:03:42 +0800

    删除webapp

--- sanjia-web/src/main/webapp/bootstrap/docs/movies/movies/moviePlate.js
+++
@@ -1,9 +0,0 @@
-    function showDiv() { 
-	  document.getElementById("bg").style.display ="block";
-	  document.getElementById("show").style.display ="block";
-    }
- 
-     function hideDiv(){
-	     document.getElementById("bg").style.display ="none";
-	     document.getElementById("show").style.display ="none";	
-     }
